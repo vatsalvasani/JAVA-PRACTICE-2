b4c522a71875ec4e9cd34f73ed683769f967e794
vatsalvasani <vatsalvasani22882@gmail.com>
2024-04-30 17:12:37 +0530

    Remove Unnecessary

---
+++ First/First.java
@@ -0,0 +1,14 @@
+package First;
+class A {
+    public static void main(String[] args)
+    {
+        System.out.println("Vatsal Here!!!");
+    }
+}
+public class First{
+
+    
+}
+class C{
+
+}
---
+++ Second/Second.java
@@ -0,0 +1,9 @@
+package Second;
+import First.*;
+public class Second {
+    public static void main(String[] args)
+    {
+        First a = new First();
+        System.out.println(a);
+    }
+}
